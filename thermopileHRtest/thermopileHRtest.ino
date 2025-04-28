2a3fd43334eb3c08bb11c477a87cd9985c892039
johnnyk2000 <108166588+johnnyk2000@users.noreply.github.com>
2025-04-28 12:10:34 -0700

    Update thermopileHRtest.ino
    now with ptat and some other minor changes i dont remember

--- thermopileHRtest/thermopileHRtest.ino
+++ thermopileHRtest/thermopileHRtest.ino
@@ -8,6 +8,7 @@
 #define tPilePin A0
 #define HRpin A1
 #define thermistorPin A2
+#define ptatPin A3
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 32 // OLED display height, in pixels
@@ -21,11 +22,16 @@ const int tPileSamples = 50;
 unsigned long timeTracker = 0;
 int threshold = 510;
 
+int starttime = 0;
+int endtime = 0;
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   pinMode(tPilePin, INPUT);
   pinMode(thermistorPin, INPUT);
+  pinMode(ptatPin, INPUT);
 
   oled.begin(&Adafruit128x32, 0x3C); //Start the OLED display
   oled.setFont(Callibri11);
@@ -50,22 +56,32 @@ void loop() {
   //   delay(10);
   // }
   // int tPileAnalog = summer1 / tPileSamples; // take avg of samples
+
+  Serial.print("Start Time: ");
+  starttime = millis();
+  Serial.print(starttime);
+
   int tPileAnalog = analogRead(tPilePin);
   int bpm = pulseSensor.getBeatsPerMinute();
   int thermistorAnalog = analogRead(thermistorPin);
+  int ptatAnalog = analogRead(ptatPin);
 
   float tPileVoltage = tPileAnalog * (5.0/1023.0);
   float thermistorVoltage = thermistorAnalog * (5.0/1023.0);
-  Serial.print("Thermopile Analog Value: ");
-  Serial.print(tPileAnalog);
+  float ptatVoltage = ptatAnalog * (5.0/1023.0);
+  //Serial.print("Thermopile Analog Value: ");
+  //Serial.print(tPileAnalog);
   Serial.print("    Thermopile Voltage Value: ");
   Serial.print(tPileVoltage, 4);
-  Serial.print("    Mistor Analog Value: ");
-  Serial.print(thermistorAnalog);
+  //Serial.print("    Mistor Analog Value: ");
+  //Serial.print(thermistorAnalog);
   Serial.print("    Mistor Voltage Value: ");
   Serial.print(thermistorVoltage, 4);
+  Serial.print ("   PTAT: ");
+  Serial.print(ptatVoltage, 4);
   Serial.print("    HR: ");
   Serial.println(bpm);
+  
 
 
 
@@ -74,14 +90,20 @@ void loop() {
     oled.setCursor(0,0);
     // oled.print(F("TP A Val: "));
     // oled.println(tPileAnalog, DEC);
-    oled.setCursor(0, 24);
+    //oled.setCursor(0, 24);
     oled.print(F("Vtp: "));
     oled.println(tPileVoltage, 4);
+
+    oled.setCursor(60, 0);
+    oled.print(F("Vam: "));
+    oled.println(thermistorVoltage, 4);
+
     // oled.print(F("TM A Val: "));
     // oled.println(thermistorAnalog, DEC);
     oled.print(F("Vam: "));
     oled.println(thermistorVoltage, 4);
-
+    oled.print(F("VPTAT: "));
+    oled.println(ptatVoltage, 4);
 
     oled.setCursor(78, 24);
     oled.print(F("HR: "));
